8d3d1219a5144b863563024d586f866adbd5f950
Bimantoro <bimantoro.widyadana@ui.ac.id>
2023-09-12 10:20:42 +0700

    change database data to dummy data

--- main/views.py
+++ main/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from .models import Item
 
 def show_main(request):
-    items = Item.objects.order_by("name")
+    # items = Item.objects.order_by("name")
+    items = [
+        Item(name="Charmander",
+             owner="Red",
+             amount=1,
+             description="Obviously prefers hot places. When it rains, steam is said to spout from the tip of its tail.")
+    ]
     context = {
         'items': items,
         'item_count': len(items)
